2c0058d7334a8c4d6b73b1ab911c5daacc17b58e
Mark Pearl <MarkPearl@gmail.com>
2012-12-19 19:08:27 +0200

    Modified scripts to create scheduler and delete scheduler

---
+++ FusionDongleSync/Tools/CreateScheduleTask.bat
@@ -0,0 +1,1 @@
+SchTasks /Create /SC MONTHLY /D 1 /TN "Fusion Dongle Update" /TR "c:\program files x86\Fusion\UpdateDongle.bat" /ST 14:00
---
+++ FusionDongleSync/Tools/DeleteScheduleTask.bat
@@ -0,0 +1,1 @@
+SchTasks /Delete /TN "Fusion Dongle Update"
